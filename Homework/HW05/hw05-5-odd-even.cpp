e59b7e53c8a448216675c314718a5048b159d3d7
zarlipzz <guitar30255@gmail.com>
2023-08-17 01:57:45 +0700

    work

--- Homework/HW05/hw05-5-odd-even.cpp
+++ Homework/HW05/hw05-5-odd-even.cpp
@@ -16,3 +16,17 @@
         -22 is an even integer
 
 */
+#include <stdio.h>
+int main()
+{
+    int num = 0;
+    printf("Input : ");
+    scanf("%d",&num);
+    if (num %2 == 0)
+    {
+        printf("%d is an even integer",num);
+    }
+    else printf("%d is an odd integer",num);
+    
+    return 0;
+}
